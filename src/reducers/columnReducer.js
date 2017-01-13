7f7e81efd9def52958fda7d4709a05d9de0f84c7
Iftekhar Rifat <rifat662@gmail.com>
2017-01-13 16:32:34 +0600

    Fix bug on deleting a column which is referenced as a foreing key in other tables

--- src/reducers/columnReducer.js
+++ src/reducers/columnReducer.js
@@ -10,8 +10,22 @@ export default (state = initialState, action) => {
             return state.deleteIn([action.id]);
         case types.SAVE_COLUMN:
             return state.setIn([action.tableId, action.data.id], fromJS(action.data));
-        case types.REMOVE_COLUMN:
-            return state.deleteIn([action.tableId, action.columnData.id]);
+        case types.REMOVE_COLUMN: {
+            // Update all columns which reference this column as a foreign key
+            const newState = state.map((table) => {
+                return table.map((column) => {
+                    const referenceId = column.getIn(['foreignKey', 'references', 'id']);
+
+                    if (referenceId === action.columnData.id) {
+                        return column.delete('foreignKey');
+                    }
+
+                    return column;
+                });
+            });
+
+            return newState.deleteIn([action.tableId, action.columnData.id]);
+        }
         case types.UPDATE_COLUMN:
             return state.setIn([action.tableId, action.data.id], fromJS(action.data));
         default:
--- src/reducers/relationReducer.js
+++ src/reducers/relationReducer.js
@@ -13,23 +13,27 @@ export default (state = initialState, action) => {
         case types.REMOVE_COLUMN: {
             // Drop all associated relations for this column
             const foreignKey = action.columnData.foreignKey;
+            const currentTableId = action.tableId;
 
             if (foreignKey !== undefined) {
-                const currentTable = action.tableId;
-                const foreignTableId = foreignKey.references.id;
+                const foreignTableId = foreignKey.on.id;
 
                 return state.filter((relation) => {
-                    return relation.get('source') !== currentTable && relation.get('target') !== foreignTableId;
+                    return relation.get('source') !== currentTableId && relation.get('target') !== foreignTableId;
+                });
+            } else {
+                return state.filter((relation) => {
+                    return relation.getIn(['data', 'on', 'id']) !== currentTableId &&
+                        relation.getIn(['data', 'references', 'id']) !== action.columnData.id;
                 });
             }
-
-            return state;
         }
         case types.SAVE_FOREIGN_KEY_RELATION:
             if (action.data !== undefined) {
                 return state.push(fromJS({
                     source: action.tableId,
-                    target: action.data.on.id
+                    target: action.data.on.id,
+                    data: action.data
                 }));
             }
 
