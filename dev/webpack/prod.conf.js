a0fa47c3ac84a1fb204d23b6c0d81b43b1725bfb
Iftekhar Rifat <rifat662@gmail.com>
2017-02-05 22:05:51 +0600

    Update production config

--- dev/webpack/prod.conf.js
+++ dev/webpack/prod.conf.js
@@ -9,11 +9,20 @@ module.exports = merge(config, {
         vendor: ['jsplumb']
     },
     plugins: plugins.concat([
+        new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
+                screw_ie8: true,
                 warnings: false,
                 drop_debugger: true
+            },
+            mangle: {
+                screw_ie8: true
+            },
+            output: {
+                comments: false,
+                screw_ie8: true
             },
             comments: false,
             sourceMap: false
