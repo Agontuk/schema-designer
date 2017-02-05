50f721e052fe0a0b8e0c3d33d94ddc63d504f07a
Iftekhar Rifat <rifat662@gmail.com>
2017-02-05 12:03:47 +0600

    Update react-redux version & add per-instance optimization for table component

--- src/containers/Table.js
+++ src/containers/Table.js
@@ -2,10 +2,23 @@
  * @flow
  */
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import Table from '../components/Table';
 
-const mapStateToProps = (state, ownProps) => ({
-    position: state.ui.positions[ownProps.data.id]
-});
+const positionSelector = () => (
+    createSelector(
+        (state, props) => state.ui.positions[props.data.id],
+        (position) => position
+    )
+);
+
+const mapStateToProps = () => {
+    // Make new copy of position for each table
+    const getPosition = positionSelector();
+
+    return (state, ownProps) => ({
+        position: getPosition(state, ownProps)
+    });
+};
 
 export default connect(mapStateToProps)(Table);
